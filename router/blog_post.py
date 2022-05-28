2745de333ae1fbbf293969c21a6ca3fe2121b520
zzlev <gpalektzja@gmail.com>
2022-05-29 00:32:15 +0900

    Add create_comment.

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import APIRouter
+from fastapi import APIRouter, Query, Body
 from pydantic import BaseModel
 
 router = APIRouter(prefix="/blog", tags=["blog"])
@@ -15,3 +15,19 @@ class BlogModel(BaseModel):
 @router.post("/new/{id}")
 def create_blog(blog: BlogModel, id: int, version: int = 1):
     return {"id": id, "data": blog, "version": version}
+
+
+@router.post("/new/{id}/comment")
+def create_comment(
+    blog: BlogModel,
+    id: int,
+    comment_id: int = Query(
+        None,
+        title="Id of the comment",
+        description="Some description for comment_id",
+        alias="commentId",
+        deprecated=True,
+    ),
+    content: str = Body(..., min_length=10, max_length=50, regex="^[a-z\s]*$"),
+):
+    return {"blog": blog, "id": id, "comment_id": comment_id, "content": content}
