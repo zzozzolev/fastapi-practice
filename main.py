ce920c9eb519171f87bba0baada903060f9cc453
zzlev <gpalektzja@gmail.com>
2022-05-28 22:01:24 +0900

    Add blog_post.router.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from router import blog_get
+from router import blog_get, blog_post
 
 app = FastAPI()
 app.include_router(blog_get.router)
+app.include_router(blog_post.router)
