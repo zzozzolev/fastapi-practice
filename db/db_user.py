d3bad9a1c2de4e6ef89d7fab1636904d0ae08833
zzlev <gpalektzja@gmail.com>
2022-05-29 23:19:12 +0900

    Add create_user to db_user.py

---
+++ db/db_user.py
@@ -0,0 +1,19 @@
+import email
+from sqlalchemy.orm.session import Session
+from db.hash import Hash
+from schemas import UserBase
+
+from db.models import DBUser
+
+
+def create_user(db: Session, request: UserBase):
+    new_user = DBUser(
+        username=request.username,
+        email=request.email,
+        password=Hash.bcrypt(request.password),
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
