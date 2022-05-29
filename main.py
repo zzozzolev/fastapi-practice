b6a1da599dd190bfc2ebc344bda6714caa9c22f7
zzlev <gpalektzja@gmail.com>
2022-05-29 23:20:26 +0900

    Add user.router to app.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from router import blog_get, blog_post
+from router import blog_get, blog_post, user
 from db import models
 from db.database import engine
 
 app = FastAPI()
+app.include_router(user.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 
