901573ef997f1e67cfb28c9da40088990b9d5f5e
zzlev <gpalektzja@gmail.com>
2022-05-29 20:06:26 +0900

    Add create_all.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from router import blog_get, blog_post
+from db import models
+from db.database import engine
 
 app = FastAPI()
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
+
+models.Base.metadata.create_all(engine)
