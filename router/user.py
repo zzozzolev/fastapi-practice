9f8a817dd8200dbca2d8eb367bf8e2c37c2afd7c
zzlev <gpalektzja@gmail.com>
2022-05-30 23:07:14 +0900

    Use UserDisplay in create_user.

--- router/user.py
+++ router/user.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from schemas import UserBase
+from schemas import UserBase, UserDisplay
 from db.database import get_db
 from db import db_user
 
 router = APIRouter(prefix="/user", tags=["user"])
 
 # Create user
-@router.post("/")
+@router.post("/", response_model=UserDisplay)
 def create_user(request: UserBase, db: Session = Depends(get_db)):
     return db_user.create_user(db, request)
 
