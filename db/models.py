83e51013c00196faf61810ff82391e7610396281
zzlev <gpalektzja@gmail.com>
2022-06-01 17:33:38 +0900

    Add DBArticle.

--- db/models.py
+++ db/models.py
@@ -1,6 +1,8 @@
 from db.database import Base
-from sqlalchemy.sql.sqltypes import Integer, String
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import Integer, String, Boolean
 from sqlalchemy import Column
+from sqlalchemy.orm import relationship
 
 
 class DBUser(Base):
@@ -9,3 +11,14 @@ class DBUser(Base):
     username = Column(String)
     email = Column(String)
     password = Column(String)
+    items = relationship("DBArticle", back_populates="user")
+
+
+class DBArticle(Base):
+    __tablename__ = "articles"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    published = Column(Boolean)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("DBUser", back_populates="items")
