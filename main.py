12c30f2602120a47f4f2c6dc0f36fb3f5ebb87f4
zzlev <gpalektzja@gmail.com>
2022-06-04 00:50:52 +0900

    Add StoryException.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from urllib.request import Request
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
+from exceptions import StoryException
 from router import blog_get, blog_post, user, article
 from db import models
 from db.database import engine
@@ -8,5 +11,13 @@ app.include_router(user.router)
 app.include_router(article.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
+
+
+@app.exception_handler(StoryException)
+def story_exception_handler(request: Request, exc: StoryException):
+    return JSONResponse(
+        status_code=status.HTTP_418_IM_A_TEAPOT, content={"detail": exc.name}
+    )
+
 
 models.Base.metadata.create_all(engine)
--- router/article.py
+++ router/article.py
@@ -1,6 +1,7 @@
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from exceptions import StoryException
 from schemas import ArticleBase, ArticleDisplay
 from db.database import get_db
 from db import db_article
@@ -10,6 +11,9 @@ router = APIRouter(prefix="/article", tags=["article"])
 # Create article
 @router.post("/", response_model=ArticleDisplay)
 def create_article(request: ArticleBase, db: Session = Depends(get_db)):
+    if request.content.startswith("Once upon a time"):
+        raise StoryException("No stories please.")
+
     return db_article.create_article(db, request)
 
 
