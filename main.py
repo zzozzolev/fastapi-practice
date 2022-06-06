8d939ee5b69d2003a957ae5bfafae12e362f7803
zzlev <gpalektzja@gmail.com>
2022-06-06 10:42:33 +0900

    Add authentication router to app.

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from fastapi import FastAPI, Request, status
 from fastapi.responses import JSONResponse
 from exceptions import StoryException
 from router import blog_get, blog_post, user, article, product
+from auth import authentication
 from db import models
 from db.database import engine
 
 app = FastAPI()
+app.include_router(authentication.router)
 app.include_router(user.router)
 app.include_router(article.router)
 app.include_router(blog_get.router)
