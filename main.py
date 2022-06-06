618ce26c53313958fc79940a40682892bebcb5f9
zzlev <gpalektzja@gmail.com>
2022-06-06 13:51:55 +0900

    Include file router.

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from urllib.request import Request
 from fastapi import FastAPI, Request, status
 from fastapi.responses import JSONResponse
 from exceptions import StoryException
-from router import blog_get, blog_post, user, article, product
+from router import blog_get, blog_post, user, article, product, file
 from auth import authentication
 from db import models
 from db.database import engine
 
 app = FastAPI()
 app.include_router(authentication.router)
+app.include_router(file.router)
 app.include_router(user.router)
 app.include_router(article.router)
 app.include_router(blog_get.router)
