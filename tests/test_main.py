67323ec8a73433da322749527d79437e335412d7
zzlev <gpalektzja@gmail.com>
2022-06-09 19:37:50 +0900

    Add test_auth_error.

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,3 +7,12 @@ client = TestClient(app)
 def test_get_all_blogs():
     response = client.get("/blog/all")
     assert response.status_code == 200
+
+
+def test_auth_error():
+    response = client.post("/token", data={"username": "", "password": ""})
+    access_token = response.json().get("access_token")
+    message = response.json().get("detail")[0].get("msg")
+
+    assert access_token == None
+    assert message == "field required"
