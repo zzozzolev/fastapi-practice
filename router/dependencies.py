93c2be18e3614b343a3324d4cafc4656e56f89d7
zzlev <gpalektzja@gmail.com>
2022-06-12 16:46:45 +0900

    Add Account class.

--- router/dependencies.py
+++ router/dependencies.py
@@ -16,3 +16,14 @@ def get_items(headers=Depends(convert_headers)):
 @router.post("/new")
 def get_items(headers=Depends(convert_headers)):
     return {"result": "new item created", "headers": headers}
+
+
+class Account:
+    def __init__(self, name: str, email: str) -> None:
+        self.name = name
+        self.email = email
+
+
+@router.post("/user")
+def create_user(name: str, email: str, password: str, account: Account = Depends()):
+    return {"name": account.name, "email": account.email}
